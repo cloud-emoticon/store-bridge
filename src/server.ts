a239c88c156ccfd724be95e81722cd6178be4aa1
KTachibanaM <whj19931115@gmail.com>
2019-07-21 22:07:27 -0700

    update

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,39 @@
 import express from 'express';
-const app = express();
+import Octokit from '@octokit/rest'
+import { File } from './api'
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const app = express();
+const github = new Octokit()
+
+app.get('/', (req, res) => res.send('Velcome to the Store'));
+
+app.get('/json', async (req, res) => {
+    const repo = await github.repos.getContents({
+        owner: 'cloud-emoticon',
+        repo: 'store-repos',
+        path: '/'
+    })
+    if (repo.status !== 200) {
+        res.status(repo.status).json([])
+        return
+    }
+
+    const data = repo.data as File[]
+    res.json(
+        data
+            .filter(d => !d.path.includes("/"))
+            .filter(d => d.path.endsWith(".json") || d.path.endsWith(".xml"))
+            .map(d => {
+                return {
+                    "name": d.path,
+                    "codeurl": `https://raw.githubusercontent.com/cloud-emoticon/store-repos/master/${d.path}`,
+                    "introduction": "N/A",
+                    "creator": "N/A",
+                    "creatorurl": "http://emoticon.moe",
+                    "iconurl": "http://emoticon.moe/img/favicon.ico"
+                }
+            })
+    )
+})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
