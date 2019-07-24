61993dd7c41a3f6b0d5d864a049225546564d92a
KTachibanaM <whj19931115@gmail.com>
2019-07-23 22:24:52 -0700

    read meta for rich info

--- src/api.ts
+++ src/api.ts
@@ -1,3 +1,17 @@
-export interface File {
+export interface GitHubFile {
     path: string
+}
+
+export interface MetaResponse {
+    name: string,
+    location: {
+        type: "localJson" | "localXml",
+        url?: string
+    },
+    description: string,
+    author: {
+        name: string,
+        url?: string,
+        avatarUrl?: string
+    }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import express from 'express';
+import rp from 'request-promise-native';
 import Octokit from '@octokit/rest'
-import { File } from './api'
+import { GitHubFile, MetaResponse } from './api'
 
 const app = express();
 const github = new Octokit()
 const port = process.env["PORT"] || 3000
 
-app.get('/', (req, res) => res.send('Velcome to the Store'));
+app.get('/', (req, res) => res.send('Willkommen to the Store'));
 
 app.get('/json', async (req, res) => {
     const repo = await github.repos.getContents({
@@ -19,22 +20,39 @@ app.get('/json', async (req, res) => {
         return
     }
 
-    const data = repo.data as File[]
-    res.json(
-        data
-            .filter(d => !d.path.includes("/"))
-            .filter(d => d.path.endsWith(".json") || d.path.endsWith(".xml"))
-            .map(d => {
-                return {
-                    "name": d.path,
-                    "codeurl": `https://raw.githubusercontent.com/cloud-emoticon/store-repos/master/${d.path}`,
-                    "introduction": "N/A",
-                    "creator": "N/A",
-                    "creatorurl": "http://emoticon.moe",
-                    "iconurl": "http://emoticon.moe/img/favicon.ico"
+    const contents = repo.data as GitHubFile[]
+    const repos = await Promise.all(contents
+        .map(file => file.path)
+        .filter(path => !path.includes("/"))
+        .filter(path => path.endsWith(".meta.json"))
+        .map(async (metaPath) => {
+            const possibleLocalFilename = metaPath.substring(0, metaPath.length - ".meta.json".length)
+            const metaUrl = `https://raw.githubusercontent.com/cloud-emoticon/store-repos/master/${metaPath}`
+            try {
+                const metaRes = await rp(metaUrl)
+                const meta = JSON.parse(metaRes) as MetaResponse
+                let codeurl
+                if (meta.location.type === "localJson") {
+                    codeurl = `https://raw.githubusercontent.com/cloud-emoticon/store-repos/master/${possibleLocalFilename}.json`
+                } else if (meta.location.type === "localXml") {
+                    codeurl = `https://raw.githubusercontent.com/cloud-emoticon/store-repos/master/${possibleLocalFilename}.xml`
                 }
-            })
+                return {
+                    "name": meta.name,
+                    "codeurl": codeurl,
+                    "introduction": meta.description,
+                    "creator": meta.author.name,
+                    "creatorurl": meta.author.url, // todo: default one
+                    "iconurl": meta.author.avatarUrl // todo: default one
+                }
+            } catch (e) {
+                console.error(`fail to obtain repo from path ${metaPath}, reason ${e}`)
+                return null
+            }
+        })
+        .filter(res => res !== null)
     )
+    res.json(repos)
 })
 
 app.listen(port, () => console.log(`listening on port ${port}`));
